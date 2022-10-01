a7a46ae6df86805c252346950ac0dbec912d82db
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-10-01 13:05:38 +0900

    tmux setup

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-tmux new-session -s trello-clone -d
-tmux split-window -v -t trello-clone # for client generate
-tmux split-window -v -t trello-clone # for client start
-tmux split-window -v -t trello-clone # for server generate
-tmux split-window -v -t trello-clone # for server start
-tmux send-keys -t trello-clone:0.1 '(cd client && npm run generate)' C-m
-tmux send-keys -t trello-clone:0.2 '(cd client && npm start)' C-m
-tmux send-keys -t trello-clone:0.3 '(cd server && npm run generate)' C-m
-tmux send-keys -t trello-clone:0.4 '(cd server && npm start)' C-m
+PWD=$(pwd)
+SESSION=$(basename "$PWD")
+
+tmux new-session -s "$SESSION" -d
+tmux split-window -v -t "$SESSION" # for client generate
+tmux select-layout even-vertical   # to avoid 'no space for new pane' 
+tmux split-window -v -t "$SESSION" # for client start
+tmux select-layout even-vertical   # to avoid 'no space for new pane' 
+tmux split-window -v -t "$SESSION" # for server generate
+tmux select-layout even-vertical   # to avoid 'no space for new pane' 
+tmux split-window -v -t "$SESSION" # for server start
+tmux select-layout even-vertical   # to avoid 'no space for new pane' 
+
+tmux send-keys -t "$SESSION:0.0" '(cd client && npm run generate)' C-m
+tmux send-keys -t "$SESSION:0.1" '(cd client && npm start)' C-m
+tmux send-keys -t "$SESSION:0.2" '(cd server && npm run generate)' C-m
+tmux send-keys -t "$SESSION:0.3" '(cd server && npm start)' C-m
