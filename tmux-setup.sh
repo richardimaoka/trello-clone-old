e1b703c8a0c0b14f1f49082cf89cbcd73661e4f8
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-11-13 19:14:46 +0900

    update tmux

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -5,18 +5,19 @@ SESSION=$(basename "$PWD")
 
 tmux new-session -s "$SESSION" -d
 
-# tmux split-window -v -t "$SESSION" # for client generate
-# tmux select-layout even-vertical   # to avoid 'no space for new pane' 
-tmux split-window -v -t "$SESSION" # for client start
+tmux split-window -v -t "$SESSION"
 tmux select-layout even-vertical   # to avoid 'no space for new pane' 
-tmux split-window -v -t "$SESSION" # for server generate
+tmux split-window -v -t "$SESSION"
 tmux select-layout even-vertical   # to avoid 'no space for new pane' 
-tmux split-window -v -t "$SESSION" # for server start
+tmux split-window -v -t "$SESSION" 
+tmux select-layout even-vertical   # to avoid 'no space for new pane' 
+tmux split-window -v -t "$SESSION" 
 tmux select-layout even-vertical   # to avoid 'no space for new pane' 
 
 tmux send-keys -t "$SESSION:0.0" '(cd client && npm run generate)' C-m
 tmux send-keys -t "$SESSION:0.1" '(cd client && npm start)' C-m
 tmux send-keys -t "$SESSION:0.2" '(cd server && npm run generate)' C-m
 tmux send-keys -t "$SESSION:0.3" '(cd server && npm start)' C-m
+tmux send-keys -t "$SESSION:0.3" '(code .)' C-m
 
 tmux attach -t "$SESSION"
