6c4759294a637216fdad9bed12e7e40b6e841c53
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-11-16 22:41:41 +0900

    load json into query

--- gqlgen/graph/resolver.go
+++ gqlgen/graph/resolver.go
@@ -1,19 +1,29 @@
 package graph
 
-import "os"
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/richardimaoka/trello-clone/gqlgen/graph/model"
+)
 
 // This file will not be regenerated automatically.
-//
 // It serves as dependency injection for your app, add any dependencies you require here.
-
-type Resolver struct{}
+type Resolver struct {
+	query struct {
+		Cards []*model.List `json:"lists"`
+	}
+}
 
 func NewResolver() *Resolver {
 	filename := "data/Query.json"
-	_, err := os.ReadFile(filename)
+	data, err := os.ReadFile(filename)
 	if err != nil {
 		panic("Failed to read: " + filename)
 	}
 
-	return &Resolver{}
+	var resolver Resolver
+	err = json.Unmarshal(data, &resolver.query)
+
+	return &resolver
 }
